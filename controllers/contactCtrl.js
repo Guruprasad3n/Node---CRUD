6386b5a707d9749d2df58cbdd41920bf58904034
Guruprasad3n <mr.guruprasadb@gmail.com>
2023-05-15 21:54:36 +0530

    commit

--- controllers/contactCtrl.js
+++ controllers/contactCtrl.js
@@ -1,37 +1,47 @@
+// const asundhandler = require("")
+const asyncHandler = require("express-async-handler")
+
+
+
 // Get All Contacts
 // Route Get /api/contacts
-const getContacts = (req, res) => {
+const getContacts = asyncHandler (async (req, res) => {
   res.status(200).json({ message: "Welcome to Home" });
-};
+});
 
 // Get Contact By Id
 // Route Get /api/contacts/:id
-const getContactById = (req, res) => {
+const getContactById = asyncHandler (async (req, res) => {
   res.status(200).json({ message: `Welcome to Home ${req.params.id} ` });
-};
+});
 // POST Create Contact
 // Route POST /api/contact
-const createContact = (req, res) => {
-    console.log(req.body)
-    const {name, email, phone} = req.body;
+const createContact = asyncHandler (async (req, res) => {
+    // console.log(req.body)
+    try{
+      const {name, email, phone} = req.body;
     if(!name || !email || !phone){
         res.status(400);
         throw new Error("All fields are manditory")
     }
   res.status(200).json({ message: "Create Request POST" });
-};
+    }
+    catch(e){
+      res.status(500).send(e)
+    }
+});
 
 // PUT Update Contact
 // Route PUT /api/contacts/:id
-const updateContact = (req, res) => {
+const updateContact = asyncHandler (async (req, res) => {
   res.status(200).json({ message: `Update  Request PUT ${req.params.id} ` });
-};
+});
 
 // Delete Contact
 // Route DELETE /api/contacts/:id
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler (async (req, res) => {
   res.status(200).json({ message: `Delete Request DELETE ${req.params.id} ` });
-};
+});
 module.exports = {
   getContacts,
   getContactById,
