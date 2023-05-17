61a3fdb9f0244503eea29111d676d6124c3e3adf
Guruprasad3n <mr.guruprasadb@gmail.com>
2023-05-17 13:49:43 +0530

    new

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv").config()
+const connectDB = require("./config/db")
 const router = require("./routes/contactRoute");
 const errorHandler = require("./middlewares/errorHandler");
 const app = express();
 
 
+connectDB()
 const PORT = process.env.PORT || 8080
 
 app.use(express.json());
